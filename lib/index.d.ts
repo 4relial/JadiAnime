4f68398e08b048119a12bb268eb3ade10a457a1b
4relial <ilham.a.pahmi@gmail.com>
2022-12-17 09:41:12 +0700

    a

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-type opts = {
-    proxyType?: string;
-    proxyUrl?: string;
-};
-export declare const JadiAnime: (img: string, opts?: opts | undefined) => Promise<{
-    img: string;
-}>;
-export {};
