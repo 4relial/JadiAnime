001c7e02cece1b05ffd4f4db528488192cdb0cf3
4relial <ilham.a.pahmi@gmail.com>
2023-05-31 10:57:50 +0700

    hjg

--- src/index.ts
+++ src/index.ts
@@ -138,7 +138,7 @@ export const JadiAnime = async (img: string, opts?: opts) => {
     return {
         code : 200, 
         img: extra.img_urls[1] as string,
-        videoUrl: qqmode === 'china' ? (extra.video_urls[0] as string) : undefined,
-        singleImg: qqmode === 'china' ? (extra.img_urls[2] as string) : undefined, 
+        videoUrl: qqmode === 'china' && extra?.video_urls[0] ? (extra.video_urls[0] as string) : undefined,
+        singleImg: qqmode === 'china' && extra?.img_urls[2] ? (extra.img_urls[2] as string) : undefined, 
     }
 };
